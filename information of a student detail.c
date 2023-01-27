bd0919b764b09c59ca95d7f7023b8f07fa92fb05
Sureka3065 <123437228+Sureka3065@users.noreply.github.com>
2023-01-27 09:58:33 +0530

    Add files via upload

---
+++ information of a student detail.c
@@ -0,0 +1,24 @@
+# include<stdio.h>
+struct student
+{
+	char name[50];
+	int roll;
+	float marks;
+}
+s;
+int main()
+{
+	printf("enter information:\n");
+	printf("enter name:");
+	fgets(s.name,sizeof(s.name),stdin);
+	printf("enter roll number:");
+	scanf("%d",&s.roll);
+	printf("enter marks:");
+	scanf("%f",&s.marks);
+	printf("displaying information:\n");
+	printf("name:");
+	printf("%s",s.name);
+	printf("roll number:%d\n",s.roll);
+	printf("marks:%f\n",s.marks);
+	return 0;
+}
